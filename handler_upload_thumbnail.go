aa627d4dc11681f016e08586e545c0a098187795
Danil Samsonov <thrashdev@protonmail.com>
2025-10-02 22:16:42 +0600

    added shit

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"encoding/base64"
+	"errors"
 	"fmt"
 	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"slices"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -42,12 +47,22 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	}
 	defer file.Close()
 
-	mediaType := header.Header.Get("Content-Type")
-	data, err := io.ReadAll(file)
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
 	if err != nil {
-		respondWithError(w, 500, "Error reading file", err)
+		respondWithError(w, 500, "Error parsing MIME media type", err)
 		return
 	}
+
+	supportedTypes := []string{"image/jpeg", "image/png"}
+	if !slices.Contains(supportedTypes, mediaType) {
+		respondWithError(w, 400, "Error parsing MIME media type", errors.New("Unsupported MIME type"))
+		return
+	}
+	// data, err := io.ReadAll(file)
+	// if err != nil {
+	// 	respondWithError(w, 500, "Error reading file", err)
+	// 	return
+	// }
 
 	dbVideo, err := cfg.db.GetVideo(videoID)
 	if err != nil {
@@ -55,16 +70,34 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	// t := thumbnail{
-	// 	data:      buf,
-	// 	mediaType: mediaType,
-	// }
-	b64 := base64.StdEncoding.EncodeToString(data)
+	ext := strings.Split(mediaType, "/")[1]
+	path := filepath.Join(cfg.assetsRoot, fmt.Sprintf("%s.%s", videoIDString, ext))
 
-	thumbnailURL := fmt.Sprintf("data:%s;base64;%s", mediaType, b64)
-	dbVideo.ThumbnailURL = &thumbnailURL
+	f, err := os.Create(path)
+	if err != err {
+		respondWithError(w, 500, "Error creating file", err)
+		return
+	}
 
+	_, err = io.Copy(f, file)
+	if err != nil {
+		respondWithError(w, 500, "Error writing to file", err)
+		return
+	}
+
+	tURL := filepath.Join("/", path)
+	dbVideo.ThumbnailURL = &tURL
+
+	// // t := thumbnail{
+	// // 	data:      buf,
+	// // 	mediaType: mediaType,
+	// // }
+	// b64 := base64.StdEncoding.EncodeToString(data)
+	//
+	// thumbnailURL := fmt.Sprintf("data:%s;base64;%s", mediaType, b64)
+	// dbVideo.ThumbnailURL = &thumbnailURL
+	//
 	cfg.db.UpdateVideo(dbVideo)
-
+	//
 	respondWithJSON(w, http.StatusOK, dbVideo)
 }
